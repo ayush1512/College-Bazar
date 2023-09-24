5951c130fa8f6cd9465a07385318294518e9d728
piyushd512 <piyushdwivedi11111@gmail.com>
2023-09-24 23:53:27 +0530

    node, mongodb, express, and ejs are added

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+app.engine('ejs', ejsMate);
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 // conecting server
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp',{ useNewUrlParser: true, 
+mongoose.connect('mongodb://127.0.0.1:27017/college-bazar',{ useNewUrlParser: true, 
 useUnifiedTopology: true})
 .then(()=>{
     console.log('MONGO open');
@@ -15,10 +24,10 @@ useUnifiedTopology: true})
 
 
 app.get('/',(req,res)=>{
-    res.send('hellow this is college bazar')
+    res.render('layouts/index.ejs')
 })
 
 // listening app that it is running
 app.listen(3000,()=>{
-    console.log('litneing 3000')
+    console.log('litneing')
 })
