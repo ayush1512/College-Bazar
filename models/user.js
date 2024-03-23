5dd878c38f752316905ac1450ad3046a682cdbc4
Saurabh Dwivedi <saurabhdwivedi01513@gmail.com>
2024-03-23 12:52:54 +0530

    commit

--- models/user.js
+++ models/user.js
@@ -1,15 +1,24 @@
-const mongoose = require('mongoose');
+const { number } = require("joi");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photos: {
+    type: String,
+    required: false,
+  },
+  phone: {
+    type: Number,
+    required: false,
+  },
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
